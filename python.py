0f25ab76831618e71da27eabfbb18552d0bbd59c
WitorZab <wiktorzabawski@gmail.com>
2024-09-17 17:08:57 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,46 @@
+# print("hello world 1 \nhello world 2")
+
+# fname = "Wiktor"
+# lname = "Zabawski"
+
+# print(f"{fname} {lname}")
+
+
+# fname = input("What is your first name? ")
+# lname = input("What is your last name? ")
+# age = input("What is your age? ")
+
+# print(f"Your full name is: {fname} {lname} \nYou are {age} years old")
+
+# age = int(input("What is your age? "))
+# score = int(input("What is your score? "))
+
+# if score > 101:
+#     print("Put your real score in")
+# elif score >= 90:
+#     if age < 10:
+#         print("Your grade is A+")
+#     else:
+#         print("Your grade is A") 
+# elif score >= 80:
+#     print('Your grade is B')
+# elif score >= 70:
+#     print("Your grade is C")
+# elif score >= 60:
+#     print("Your grade is D")
+# else:
+#     print("Your grade is F")
+
+#FIZZBUZZ
+
+# for num in range(1, 100):
+#     if num % 5 == 0 and num % 3 == 0:
+#         print("fizzbuzz")
+#     elif num % 3 == 0:
+#         print("fiz")
+#     elif num % 5 == 0:
+#         print("buzz")
+#     else:
+#         print(num)
+ 
+
